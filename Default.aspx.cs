c1102cbcba3865cb1e3f041cf5ef2b335639bea0
New Future <new_future@msn.cn>
2016-04-25 11:08:57 +0800

    增加打印页码支持

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,13 +7,14 @@
 using System;
 using System.Diagnostics;
 using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
 
 public partial class Default : System.Web.UI.Page
 {
     /// <summary>
     /// 上传密码
     /// </summary>
-    protected string Password = "NewFuture";
+    protected string Password = "打印密码";
     /// <summary>
     /// 允许的文件后缀
     /// </summary>
@@ -28,6 +29,7 @@ public partial class Default : System.Web.UI.Page
     {
         if (FileUpload.HasFile)
         {
+            LabelMsg.ForeColor = System.Drawing.Color.Red;
             if (pwdTextBox.Text != Password)
             {
                 LabelMsg.Text = "上传密码无效!";
@@ -39,16 +41,25 @@ public partial class Default : System.Web.UI.Page
             }
             else
             {
+                //打印范围
+                string range = GetRange();
+                if (range == null)
+                {
+                    LabelMsg.Text = FileUpload.FileName + "打印页码范围 "+rangeInput.Text+" 格式错误！";
+                    return;
+                }
                 //保存并打印上传文件
                 Random r = new Random();
                 string path = Server.MapPath("~/file/") + DateTime.UtcNow.ToString("MM-dd_HH-mm-ss_") + r.Next().ToString() + GetType(FileUpload.FileName);
                 FileUpload.SaveAs(path);
                 int copies = 1;
                 int.TryParse(copy.Text, out copies);
+
                 //打印
-                if (print(path, copies))
+                if (print(path, copies, range))
                 {
-                    LabelMsg.Text = FileUpload.FileName + "[" + copies + "份]已经添加至打印队列~";
+                    LabelMsg.Text = FileUpload.FileName + "[" + copies + "份 " + range + "]已经添加至打印队列~";
+                    LabelMsg.ForeColor = System.Drawing.Color.Green;
                 }
                 else
                 {
@@ -72,20 +83,39 @@ public partial class Default : System.Web.UI.Page
         string ext = System.IO.Path.GetExtension(filename).ToLower();
         return Array.IndexOf(AllowType, ext) == -1 ? null : ext;
     }
+    private string GetRange()
+    {
+        string range = rangeInput.Text ?? "";
+        if (string.IsNullOrEmpty(range))
+        {
+            range = "";
+        }
+        else
+        {
+            string expr = @"^((\d+(\-\d+)?)\,)*\d+(\-\d+)?$";//^((\d+(\-\d+)?)\,)*\d+(\-\d+)?$
+            range = range.Trim().Replace(' ', ',').Replace("，", ",").Trim(',');
+            if (!Regex.IsMatch(range, expr))
+            {
+                return null;
+            }
+        }
+        return range;
+    }
 
     /// <summary>
     /// 打印
+    /// https://github.com/sumatrapdfreader/sumatrapdf/wiki/Command-line-arguments
     /// </summary>
     /// <param name="path"></param>
     /// <param name="copy"></param>
-    /// <param name="printer"></param>
+    /// <param name="range">范围,1-5</param>
     /// <returns></returns>
-    protected bool print(string path, int copy = 1)
+    protected bool print(string path, int copy = 1, string range = null)
     {
         try
         {
             var cmd = Server.MapPath("~/bin/") + "SumatraPDF.exe";
-            var param = string.Format("-print-to-default -print-settings \"{0}x\" \"{1}\"", copy, path);
+            var param = string.Format("-print-to-default -print-settings \"{0}x,{1}\" \"{2}\"", copy, range, path);
             var p = Process.Start(cmd, param);
             return true;
         }
