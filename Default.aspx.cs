d534ef25f5f29b89f913b6621957e3f1ce359685
New Future <github@newfuture.cc>
2017-05-24 23:11:29 +0800

    支持word打印

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -18,7 +18,7 @@ public partial class Default : System.Web.UI.Page
     /// <summary>
     /// 允许的文件后缀
     /// </summary>
-    protected string[] AllowType = { ".pdf", ".jpg", ".png", ".tiff" };
+    protected string[] AllowType = { ".pdf", ".jpg", ".png", ".tiff", ".doc", ".docx", ".rtf", ".txt" };
 
     /// <summary>
     /// 上传打印
@@ -30,11 +30,12 @@ public partial class Default : System.Web.UI.Page
         if (FileUpload.HasFile)
         {
             LabelMsg.ForeColor = System.Drawing.Color.Red;
+            string type = GetType(FileUpload.FileName);
             if (pwdTextBox.Text != Password)
             {
                 LabelMsg.Text = "上传密码无效!";
             }
-            else if (GetType(FileUpload.FileName) == null)
+            else if (type == null)
             {
                 LabelMsg.Text = "上传失败，不是PDF或者指定的图片文件：" + FileUpload.FileName;
                 return;
@@ -45,7 +46,7 @@ public partial class Default : System.Web.UI.Page
                 string range = GetRange();
                 if (range == null)
                 {
-                    LabelMsg.Text = FileUpload.FileName + "打印页码范围 "+rangeInput.Text+" 格式错误！";
+                    LabelMsg.Text = FileUpload.FileName + "打印页码范围 " + rangeInput.Text + " 格式错误！";
                     return;
                 }
                 //保存并打印上传文件
@@ -56,7 +57,7 @@ public partial class Default : System.Web.UI.Page
                 int.TryParse(copy.Text, out copies);
 
                 //打印
-                if (print(path, copies, range))
+                if (print(path, copies, range, type))
                 {
                     LabelMsg.Text = FileUpload.FileName + "[" + copies + "份 " + range + "]已经添加至打印队列~";
                     LabelMsg.ForeColor = System.Drawing.Color.Green;
@@ -104,19 +105,38 @@ public partial class Default : System.Web.UI.Page
 
     /// <summary>
     /// 打印
-    /// https://github.com/sumatrapdfreader/sumatrapdf/wiki/Command-line-arguments
     /// </summary>
     /// <param name="path"></param>
     /// <param name="copy"></param>
     /// <param name="range">范围,1-5</param>
     /// <returns></returns>
-    protected bool print(string path, int copy = 1, string range = null)
+    protected bool print(string path, int copies = 1, string range = null, string type = ".pdf")
     {
+        string cmd, param;
+        switch (type)
+        {
+            case ".pdf":
+            case ".jpg":
+            case ".png":
+            case ".tiff":
+                //pdf
+                // https://github.com/sumatrapdfreader/sumatrapdf/wiki/Command-line-arguments
+                cmd = Server.MapPath("~/bin/") + "SumatraPDF.exe";
+                param = string.Format("-print-to-default -print-settings \"x,{0}\" \"{1}\"", range, path);
+                break;
+
+            default:
+                //word
+                cmd = "write";
+                param = string.Format("/p \"{0}\"", path);
+                break;
+        }
         try
         {
-            var cmd = Server.MapPath("~/bin/") + "SumatraPDF.exe";
-            var param = string.Format("-print-to-default -print-settings \"{0}x,{1}\" \"{2}\"", copy, range, path);
-            var p = Process.Start(cmd, param);
+            while (copies-- > 0)
+            {
+                Process.Start(cmd, param);
+            }
             return true;
         }
         catch (Exception ex)
